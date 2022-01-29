34bed4a88575b164ea82b3e184a3346edfd90b56
Bradley Shellnut <bradleyshellnut@protonmail.com>
2022-01-28 16:08:32 -0800

    Created basic loader using stores on Svelte Kit.

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { navigating } from '$app/stores';
+  
+  import { loading } from '$lib/loading';
+  import Loading from '$lib/Loading.svelte';
+
+  $: $loading = !!$navigating;
+</script>
+
+{$loading}
+
+<slot />
+
+<button on:click={() => $loading = true}>Set Loading</button>
+
+<button on:click={() => $loading = false}>Stop Loading</button>
+
+<Loading />
